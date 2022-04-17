8d683aee3b6b7fe329496736deb4e50c5235b0a3
GonzaloBelvisi <gonzalobelvisi@gmail.com>
2022-04-17 16:59:56 -0300

    commit

--- src/components/pages/Servicios.jsx
+++ src/components/pages/Servicios.jsx
@@ -8,7 +8,7 @@ export default function Servicios() {
       <article className="item-services">
           <img src={ServicioTecnico} alt="Servicio Tecnico"></img>
           <h2>Servicio Técnico</h2>
-          <p>TecnoBelvi provee servicio de venta, desarrollo y manejo de soluciones, soporte técnico tanto correctivo como preventivo, suministramos soluciones en equipos de cómputo, periféricos y software especializado, para todo tipo de plataformas.
+          <p>TecnoBelvi provee servicio de venta. desarrollo y manejo de soluciones, soporte técnico tanto correctivo como preventivo, suministramos soluciones en equipos de cómputo, periféricos y software especializado, para todo tipo de plataformas.
               Como distribuidores y centros autorizados de servicio manejamos toda la linea de productos, accesorios compatibles y contamos con el mejor soporte en garantías directas.
           </p>
           <p>Cualquier consulta o pedido al: +598 99 244 062</p>
